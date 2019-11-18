3ad6cc6efe22f1c74950850c219d7fee09994c44
Devon James Wright <devonjamess.wright@gmail.com>
2019-11-18 13:44:39 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -5,5 +5,5 @@ sum = first_number + second_number
 third_number = 12
 fourth_number = 45
 difference = first_number - second_number
-multiply = first_number * second_number
-dividend = first_number second_number
+product = first_number * second_number
+quotient = first_number second_number
